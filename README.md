2f05bdd9802e42d66ec38022da3686c6d7413761
sapate vaibhav <64630274+sapatevaibhav@users.noreply.github.com>
2023-11-15 09:09:33 +0530

    Added all files

---
+++ README.md
@@ -0,0 +1,17 @@
+# auth_actor
+
+Authentication Application built using flutter
+
+This is a simple Authenticator app which uses ```local_auth``` flutter package to verify the user's identity.
+As per the package name I think nothing is sent elsewhere except the device. 
+
+This application provides features to:
+- Check if authentication is available
+- List al the biometric types available
+- Check for the authentication
+- Report if authentication was successfull or not.
+
+This is my second project as Android Development Intern at CodeClause.
+
+If this project provides any kind of knowledge to you then do what you want to do. I am happy that you found this project as useful.<br>
+Thanks...
---
+++ lib/main.dart
@@ -0,0 +1,177 @@
+// ignore_for_file: prefer_const_constructors, use_build_context_synchronously
+
+import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(
+        title: 'Authenticator',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final LocalAuthentication _localAuthentication = LocalAuthentication();
+  bool _canCheckBiometric = false;
+  String _authorizedOrNot = "Not Authorized";
+  List<BiometricType> _availableTypes = List.empty();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Divider(),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: _checkBiometric,
+              child: Text("Check for Biometric"),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Is Biometric Supported? $_canCheckBiometric",
+              style: TextStyle(
+                  color: _canCheckBiometric ? Colors.green : Colors.red),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Divider(),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: _getListOfBiometricTypes,
+              child: Text("List Biometrics available"),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(100, 0, 100, 0),
+              child: Text(
+                "List of available biometrics ${_availableTypes.toString()}",
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Divider(),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Click here to authorize now",
+            ),
+            ElevatedButton(
+              onPressed: _authorizeNow,
+              child: Text("Authorize Now"),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Authorized? $_authorizedOrNot",
+              style: TextStyle(
+                  color: _authorizedOrNot == "Authorized"
+                      ? Colors.green
+                      : Colors.red),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Divider(),
+            SizedBox(
+              height: 100,
+            ),
+            Text(
+              "made by sapatevaibhav 🧡",
+              style: TextStyle(fontSize: 10),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _checkBiometric() async {
+    bool canCheckBiometric = false;
+    try {
+      canCheckBiometric = await _localAuthentication.canCheckBiometrics ||
+          await _localAuthentication.isDeviceSupported();
+    } on PlatformException {
+      //
+    }
+    if (!mounted) return;
+    setState(() {
+      _canCheckBiometric = canCheckBiometric;
+    });
+  }
+
+  Future<void> _getListOfBiometricTypes() async {
+    List<BiometricType> listOfBiometrics = List.empty();
+    try {
+      listOfBiometrics = await _localAuthentication.getAvailableBiometrics();
+    } on PlatformException {
+      //
+    }
+    if (!mounted) return;
+    setState(() {
+      _availableTypes = listOfBiometrics;
+    });
+  }
+
+  Future<void> _authorizeNow() async {
+    bool isAuthorized = false;
+    try {
+      isAuthorized = await _localAuthentication.authenticate(
+        localizedReason:
+            "\n\nDear user this process is only for test purpose \n\nThe application needs to check for the biometrics",
+      );
+    } on PlatformException {
+      //
+    }
+    if (!mounted) return;
+    setState(() {
+      if (isAuthorized) {
+        _authorizedOrNot = "Authorized";
+      } else {
+        _authorizedOrNot = "Not Authorized";
+      }
+    });
+  }
+}
